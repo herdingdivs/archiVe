e4ce914956c90fed355face176413bb1fcbc0a67
ysjoyce <yvonnejoyce@comcast.net>
2015-05-19 13:42:54 -0600

    Limited results to 8. Need to work on conforming tile size.

--- php/searchResults.php
+++ php/searchResults.php
@@ -8,6 +8,7 @@
       <div class="col-xs-12 col-sm-6 col-md-3"></div>
       <div class="col-xs-12 col-sm-6 col-md-3"></div>
       <div class="col-xs-12 col-sm-6 col-md-3"></div>
+      <br>
       <div class="col-xs-12 col-sm-6 col-md-3"></div>
       <div class="col-xs-12 col-sm-6 col-md-3"></div>
       <div class="col-xs-12 col-sm-6 col-md-3"></div>
